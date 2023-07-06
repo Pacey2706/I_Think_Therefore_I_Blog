9fe1c70c61507aa5d939ad232f6de13ec786f341
Pacey2706 <pacey2706@gmail.com>
2023-07-06 19:53:05 +0000

    update allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['aqueous-wave-83183.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['aqueous-wave-83183-c699ff9f13e2.herokuapp.com', 'localhost']
 
 
 # Application definition
